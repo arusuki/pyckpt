f44feaafe44e380ab088c6beca35b50eb891e88e
tao-lintian <wojiaowenzhong233@outlook.com>
2025-05-11 13:22:12 +0800

    ♻️remove unused `RecursiveDict`

--- pyckpt/objects.py
+++ pyckpt/objects.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, Generator, List, Optional, Tuple, Type
+from typing import Any, Callable, Dict, Generator, List, Tuple, Type
 
 LoadHook = Callable[[Any], Any]
 StoreHook = Callable[[Any, Dict], Any]
@@ -10,53 +10,6 @@ class NullObjectType:
 
 
 NullObject = NullObjectType()
-
-
-class RecursiveDict:
-    def __init__(self, parent: Optional[Dict]):
-        if parent is None:
-            parent = {}
-        self._parent = parent
-        self._local = {}
-
-    def __getitem__(self, key: Any) -> Any:
-        if key in self._local:
-            return self._local[key]
-        return self._parent[key]
-
-    def __setitem__(self, key: Any, value: Any) -> None:
-        self._local[key] = value
-
-    def __delitem__(self, key: Any) -> None:
-        if key in self._local:
-            del self._local[key]
-        else:
-            raise KeyError(f"Key '{key}' not found in local dictionary.")
-
-    def __contains__(self, key: Any) -> bool:
-        return key in self._local or key in self._parent
-
-    def get(self, key: Any, default: Any = None) -> Any:
-        if key in self._local:
-            return self._local[key]
-        return self._parent.get(key, default)
-
-    def keys(self):
-        return set(self._local.keys()).union(self._parent.keys())
-
-    def items(self):
-        combined = {**self._parent, **self._local}
-        return combined.items()
-
-    def values(self):
-        combined = {**self._parent, **self._local}
-        return combined.values()
-
-    def __iter__(self):
-        return iter(self.keys())
-
-    def __len__(self):
-        return len(self.keys())
 
 
 class StubObject:
