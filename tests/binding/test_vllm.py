4936aaf2091b8a7dc71b8ba2a154e6d506241b95
Tao-lintian <40430743+arusuki@users.noreply.github.com>
2025-07-29 01:20:19 +0800

    🔧basic `MultiProcExecutor` support for vllm(without pp) (#11)
    * 🔧implement MultiProcExecutor support

--- tests/binding/test_vllm.py
+++ tests/binding/test_vllm.py
@@ -6,33 +6,26 @@ from io import BytesIO, StringIO
 from multiprocessing import Process
 from typing import Any, Callable, Optional
 
-import dill
 import torch
 from dill import Unpickler
 from tokenizers.decoders import DecodeStream
 from torch.multiprocessing import Queue
 from transformers import AutoTokenizer
 from vllm.engine.arg_utils import EngineArgs
-from vllm.executor.uniproc_executor import UniProcExecutor
 from vllm.sampling_params import RequestOutputKind, SamplingParams
 from vllm.transformers_utils.tokenizer_group import TokenizerGroup
-from vllm.v1.core.sched.scheduler import Scheduler
 from vllm.v1.engine import EngineCoreRequest
 from vllm.v1.engine.core import EngineCore
 from vllm.v1.engine.output_processor import OutputProcessor
 from vllm.v1.executor.abstract import Executor
-from vllm.v1.worker.gpu_model_runner import GPUModelRunner
 
 from pyckpt import objects
 from pyckpt.binding import torch as patch_torch
 from pyckpt.binding import vllm as patch_vllm
 from pyckpt.binding.vllm import (
-    get_cache_blocks,
-    get_cache_tensors,
     prepare_engine,
     reduce_decode_stream,
     reduce_engine_core,
-    set_cache_blocks,
 )
 from pyckpt.objects import Pickler
 from tests.utils import (
@@ -43,22 +36,24 @@ from tests.utils import (
 )
 
 # MODEL_NAME =  "/home/yuuka/testp/Qwen2.5-7B-Instruct-GPTQ-Int8"
-MODEL_NAME =  "/docker/data/HF_MODELS/Qwen2.5-7B-Instruct-GPTQ-Int8"
+MODEL_NAME = "/docker/data/HF_MODELS/Qwen2.5-7B-Instruct-GPTQ-Int8"
 # MODEL_NAME =  "/home/yuuka/testp/opt-125m"
 PROMPT = "implement quick sort in C programming language: "
-TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
-PROMPT_TOKENS = TOKENIZER(PROMPT).input_ids
 
-print(f"prompt tokens: {len(PROMPT_TOKENS)}")
 
 def make_request() -> EngineCoreRequest:
+    TOKENIZER = AutoTokenizer.from_pretrained(MODEL_NAME)
+    PROMPT_TOKENS = TOKENIZER(PROMPT).input_ids
+    print(f"prompt tokens: {len(PROMPT_TOKENS)}")
     return EngineCoreRequest(
         request_id=str(uuid.uuid4()),
         prompt_token_ids=PROMPT_TOKENS,
         mm_inputs=None,
         mm_hashes=None,
         mm_placeholders=None,
-        sampling_params=SamplingParams(output_kind=RequestOutputKind.DELTA, max_tokens=2048),
+        sampling_params=SamplingParams(
+            output_kind=RequestOutputKind.DELTA, max_tokens=2048
+        ),
         eos_token_id=None,
         arrival_time=time.time(),
         lora_request=None,
@@ -66,11 +61,13 @@ def make_request() -> EngineCoreRequest:
         data_parallel_rank=None,
     )
 
+
 def join_safe(process: Process):
     process.join()
     assert process.exitcode == 0
 
-def _make_engine_core():
+
+def _make_engine_core(tp_size=1):
     os.environ["VLLM_USE_V1"] = "1"
     engine_args = EngineArgs(
         model=MODEL_NAME,
@@ -78,13 +75,10 @@ def _make_engine_core():
         enforce_eager=True,
         max_model_len=8192,
         # gpu_memory_utilization=0.3,
+        tensor_parallel_size=tp_size,
     )
     vllm_config = engine_args.create_engine_config()
-
-    print("static_forward_context: ", vllm_config.compilation_config.static_forward_context)
-
     assert vllm_config.compilation_config.level == 0
-
     executor_class = Executor.get_class(vllm_config)
     engine_core = EngineCore(
         vllm_config=vllm_config,
@@ -92,6 +86,7 @@ def _make_engine_core():
         log_stats=False,
     )
     return engine_core
+
 
 def _step_engine(core: EngineCore):
     core.add_request(make_request())
@@ -102,6 +97,7 @@ def _step_engine(core: EngineCore):
     assert len(core.scheduler.waiting) == 0
     assert len(core.scheduler.running) == 1
 
+
 def _prepare_and_step_engine():
     core = _make_engine_core()
     with prepare_engine(core):
@@ -109,21 +105,29 @@ def _prepare_and_step_engine():
     _step_engine(core)
 
 
-def _test_reduce_engine(q: Queue, test_func: Optional[Callable[[EngineCore], Any]] = None):
-    patch_torch.init()
-    core = _make_engine_core()
-    ret = None
-    if test_func:
-        ret = test_func(core)
-    file = BytesIO()
-    pickler = Pickler(file)
-    pickler.dispatch_table[EngineCore] = reduce_engine_core
-    pickler.dispatch_table[DecodeStream] = reduce_decode_stream
-    pickler.dump(ret)
-    pickler.dump(core)
-    storages = pickler.consume_persisted()
-    engine_data = file.getvalue()
-    q.put((engine_data, storages))
+def _test_reduce_engine(
+    q: Queue,
+    test_func: Optional[Callable[[EngineCore], Any]] = None,
+    tp_size=1,
+):
+    try:
+        patch_torch.init()
+        core = _make_engine_core(tp_size)
+        ret = None
+        if test_func:
+            ret = test_func(core)
+        file = BytesIO()
+        pickler = Pickler(file)
+        pickler.dispatch_table[EngineCore] = reduce_engine_core
+        pickler.dispatch_table[DecodeStream] = reduce_decode_stream
+        pickler.dump(ret)
+        pickler.dump(core)
+        storages = pickler.consume_persisted()
+        engine_data = file.getvalue()
+        q.put((engine_data, storages))
+    finally:
+        core.shutdown()
+
 
 def _test_rebuild_engine(
     engine_data: bytes,
@@ -131,25 +135,32 @@ def _test_rebuild_engine(
     test_func: Callable[[EngineCore], None],
     q: Optional[Queue] = None,
 ):
-    unpickler = objects.Unpickler(BytesIO(engine_data), storages)
-    args = unpickler.load()
-    core = unpickler.load()
-    if args:
-        ret = test_func(core, args)
-    else:
-        ret = test_func(core)
+    core: Optional[EngineCore] = None
+    try:
+        unpickler = objects.Unpickler(BytesIO(engine_data), storages)
+        args = unpickler.load()
+        core = unpickler.load()
+        if args:
+            ret = test_func(core, args)
+        else:
+            ret = test_func(core)
 
-    if q:
-        file = BytesIO()
-        pickler = Pickler(file)
-        pickler.dispatch_table[EngineCore] = reduce_engine_core
-        pickler.dispatch_table[DecodeStream] = reduce_decode_stream
-        pickler.dump(ret)
-        q.put(file.getvalue())
+        if q:
+            file = BytesIO()
+            pickler = Pickler(file)
+            pickler.dispatch_table[EngineCore] = reduce_engine_core
+            pickler.dispatch_table[DecodeStream] = reduce_decode_stream
+            pickler.dump(ret)
+            q.put(file.getvalue())
+    finally:
+        if core:
+            core.shutdown()
+
 
 def test_vllm_prepare_engine():
     ut = run_spawned(_prepare_and_step_engine)
     join_safe(ut)
+
 
 def test_vllm_reduce_engine():
     q = make_queue()
@@ -160,6 +171,7 @@ def test_vllm_reduce_engine():
 
     reloader = run_spawned(_test_rebuild_engine, engine_data, storages, _step_engine)
     join_safe(reloader)
+
 
 def _step_engine_and_process(
     num_step: int,
@@ -200,22 +212,24 @@ def _step_engine_and_process(
 
     return processor, save_random_states(), output
 
-def print_req_blocks(core: EngineCore):
-    scheduler = core.scheduler
-    assert isinstance(scheduler, Scheduler)
-    executor = core.model_executor
-    assert isinstance(executor, UniProcExecutor)
-    model_runner = executor.driver_worker.worker.model_runner
-    assert isinstance(model_runner, GPUModelRunner)
-    cached_req_states = model_runner.requests
 
-    for req, cached_req in zip(scheduler.requests, cached_req_states.values()):
-        print(
-            "cache blocks(manager): ", scheduler.kv_cache_manager.get_block_ids(req)
-        )
-        print(
-            "cache blocks(model_runner): ", cached_req.block_ids
-        )
+def print_req_blocks(core: EngineCore):
+    # scheduler = core.scheduler
+    # assert isinstance(scheduler, Scheduler)
+    # executor = core.model_executor
+    # assert isinstance(executor, UniProcExecutor)
+    # model_runner = executor.driver_worker.worker.model_runner
+    # assert isinstance(model_runner, GPUModelRunner)
+    # cached_req_states = model_runner.requests
+    #
+    # for req, cached_req in zip(scheduler.requests, cached_req_states.values()):
+    #     print(
+    #         "cache blocks(manager): ", scheduler.kv_cache_manager.get_block_ids(req)
+    #     )
+    #     print(
+    #         "cache blocks(model_runner): ", cached_req.block_ids
+    #     )
+    pass
 
 
 def print_cache_blocks(cache_blocks: list[list[tuple[int, torch.Tensor]]]):
@@ -242,9 +256,9 @@ def test_reduce_output_processor():
     file.seek(0)
     processor = Unpickler(file).load()
     assert isinstance(processor, OutputProcessor)
-    
+
+
 def test_vllm_engine_step_after_dump():
-    
     def get_text_from_data(engine_data: bytes):
         pickler = Unpickler(BytesIO(engine_data))
         ret = pickler.load()
@@ -277,41 +291,33 @@ def test_vllm_engine_step_after_dump():
     interrupted = get_text_from_data(engine_data)
     print(interrupted)
 
-    # at this time we cannot ensure 100% state recover
     assert reference == interrupted
 
-def _get_cache_blocks(q: Queue):
-    core = _make_engine_core()
-    core.add_request(make_request())
-    for _ in range(32):
-        print(core.step())
-    tensors = get_cache_tensors(core)
-    cache_blocks = get_cache_blocks(core)
-    q.put((dill.dumps(tensors), dill.dumps(cache_blocks)))
 
-def _reload_and_compare(tensor_data: bytes, cache_blocks_data: bytes):
-    tensors = dill.loads(tensor_data)
-    cache_blocks = dill.loads(cache_blocks_data)
+def test_vllm_reduce_engine_tp():
+    def get_text_from_data(engine_data: bytes):
+        pickler = Unpickler(BytesIO(engine_data))
+        ret = pickler.load()
+        assert isinstance(ret, tuple) and len(ret) == 3
+        output = ret[2]
+        assert isinstance(output, StringIO)
+        return output.getvalue()
 
-    core = _make_engine_core()
-    set_cache_blocks(core, cache_blocks)
-    new_tensors = get_cache_tensors(core)
-    assert len(tensors) == len(new_tensors)
-    for tensor_group, new_tensor_group in zip(tensors, new_tensors):
-        assert len(tensor_group) == len(new_tensor_group)
-        for tensor, new_tensor in zip(tensor_group, new_tensor_group):
-            assert torch.equal(tensor[:, :500], new_tensor[:, :500])
+    def step(num_step: int):
+        return partial(_step_engine_and_process, num_step)
 
-def test_vllm_get_cache_blocks():
     q = make_queue()
-    step = run_spawned(_get_cache_blocks, q)
-    tensor_data, cache_blocks_data = q.get()
-    join_safe(step)
-    
-    reload = run_spawned(
-        _reload_and_compare,
-        tensor_data,
-        cache_blocks_data,
-    )
-    join_safe(reload)
+    dumper = run_spawned(_test_reduce_engine, q, step(64), 2)
+    engine_data, storages = q.get()
+    join_safe(dumper)
+
+    reference = get_text_from_data(engine_data)
+    print(reference)
+
+    reloader = run_spawned(_test_rebuild_engine, engine_data, storages, step(64), q)
+    engine_data = q.get()
+    join_safe(reloader)
+
+    interrupted = get_text_from_data(engine_data)
+    print(interrupted)
 
